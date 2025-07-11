9e9ea8fe53fa8dafcb64f45f331b08467e2450ba
mariati88 <151051257+mariati88@users.noreply.github.com>
2025-07-11 16:45:31 +0700

    Refactor login command; streamline user data retrieval and improve code readability

--- cypress/e2e/CheckoutProduct.cy.js
+++ cypress/e2e/CheckoutProduct.cy.js
@@ -1,8 +1,22 @@
 describe('checkout product', () => {
   beforeEach(() => {
-    cy.login()  // memanggil reusable login dari commands.js
+    cy.login() // memanggil reusable login dari commands.js 
   })
   it('TC_Chekout_001 - Select and checkout product', () => {
-    
+  cy.get('.features_items > :nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click()
+  cy.get('[href="/product_details/1"]').click()
+  cy.get('#quantity').clear().type('3')
+  cy.wait(500) //agar value nya diproses
+  cy.contains('Add to cart').click()
+  cy.contains('View Cart').click()
+  cy.contains('Proceed To Checkout').click()
+  cy.contains('Place Order').click()
+  cy.get('[data-qa="name-on-card"]').type('Maria')
+  cy.get('[data-qa="card-number"]').type('1234' )
+  cy.get('[data-qa="cvc"]').type('228' )
+  cy.get('[data-qa="expiry-month"]').type('10')
+  cy.get('[data-qa="expiry-year"]').type('2026')
+  cy.get('[data-qa="pay-button"]').click()
+  cy.contains('Order Placed!').should('be.visible')              
   })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,11 @@
-cypress.commands.add('login',(email,password)=>{
+Cypress.Commands.add('login',(email,password)=>{
     cy.fixture('userData').then((user) => {
     cy.visit('https://automationexercise.com')
     cy.get('.shop-menu > .nav > :nth-child(4)').click() 
-    cy.get('[data-qa="login-email"]').type(email)
-    cy.get('[data-qa="login-password"]').type(password)
+    cy.get('[data-qa="login-email"]').type(user.email)
+    cy.get('[data-qa="login-password"]').type(user.password)
     cy.get('[data-qa="login-button"]').click()
 })
 })
+
+
