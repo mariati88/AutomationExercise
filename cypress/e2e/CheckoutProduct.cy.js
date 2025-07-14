5bde070278ba683fadf8c1d648eef2f4c5348c40
mariati88 <151051257+mariati88@users.noreply.github.com>
2025-07-14 11:26:46 +0700

    Refactor checkout tests; improve code structure and enhance readability

--- cypress/e2e/CheckoutProduct.cy.js
+++ cypress/e2e/CheckoutProduct.cy.js
@@ -3,20 +3,49 @@ describe('checkout product', () => {
     cy.login() // memanggil reusable login dari commands.js 
   })
   it('TC_Chekout_001 - Select and checkout product', () => {
-  cy.get('.features_items > :nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click()
   cy.get('[href="/product_details/1"]').click()
-  cy.get('#quantity').clear().type('3')
-  cy.wait(500) //agar value nya diproses
+  cy.get('#quantity')
+    .clear()
+    .wait(500) //agar dapat value nya diproses
+    .type('3')
+    .blur()
+    .should('have.value','3')
   cy.contains('Add to cart').click()
   cy.contains('View Cart').click()
   cy.contains('Proceed To Checkout').click()
   cy.contains('Place Order').click()
-  cy.get('[data-qa="name-on-card"]').type('Maria')
+  cy.get('[data-qa="name-on-card"]').type('card123')
   cy.get('[data-qa="card-number"]').type('1234' )
   cy.get('[data-qa="cvc"]').type('228' )
   cy.get('[data-qa="expiry-month"]').type('10')
   cy.get('[data-qa="expiry-year"]').type('2026')
   cy.get('[data-qa="pay-button"]').click()
-  cy.contains('Order Placed!').should('be.visible')              
+  cy.contains('Order Placed!').should('be.visible')   
   })
+
+  it('TC_Chekout_002 - empty form payment', () => {
+  cy.get('[href="/product_details/1"]').click()
+  cy.contains('Add to cart').click()
+  cy.contains('View Cart').click()
+  cy.contains('Proceed To Checkout').click()
+  cy.contains('Place Order').click()
+  cy.get('[data-qa="name-on-card"]').should(($input) => {
+  expect($input[0].checkValidity()).to.be.false
+   })
+   })
+   })
+
+  describe('checkout product - guest user', () => {
+  it('TC_Chekout_003 - Checkout Product without login', () => {
+  cy.visit('https://automationexercise.com')
+  cy.contains('Products').click()
+  cy.get(':nth-child(3) > .product-image-wrapper > .choose > .nav > li > a').click()
+  cy.contains('Add to cart').click()
+  cy.get('u').click()
+  cy.contains('Proceed To Checkout').click()
+  cy.get('.modal-content').should('be.visible')
   })
+   })
+  
+
+ 
